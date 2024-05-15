783b3d3c0ecf81a0b97286f8f365cb14a66d7c34
Max Kasperowicz <yet3.dev@gmail.com>
2024-05-15 18:32:51 +0200

    add plugin for formating qmk

---
+++ nvim/lua/plugins/qmk.lua
@@ -0,0 +1,20 @@
+return {
+  "codethread/qmk.nvim",
+  opts = {
+    name = 'LAYOUT_5x6',
+    layout = {
+      'x x x x x x _ _ _ _ _ x x x x x x',
+      'x x x x x x _ _ _ _ _ x x x x x x',
+      'x x x x x x _ _ _ _ _ x x x x x x',
+      'x x x x x x _ _ _ _ _ x x x x x x',
+      '_ _ x x _ _ _ _ _ _ _ _ _ x x _ _',
+      '_ _ _ _ x x _ _ _ _ _ x x _ _ _ _',
+      '_ _ _ _ _ _ x x _ x x _ _ _ _ _ _',
+      '_ _ _ _ _ _ x x _ x x _ _ _ _ _ _',
+    }
+  },
+  config = function(M, opts)
+    local qmk = safe_plug_load('qmk', M)
+    qmk.setup(opts)
+  end
+}
