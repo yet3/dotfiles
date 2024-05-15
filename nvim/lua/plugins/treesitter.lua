a58df024c23338deb02ff936dcdd4ca3f8a8714b
Max Kasperowicz <yet3.dev@gmail.com>
2024-05-15 22:32:31 +0200

    use 0.9.2 version of treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 local load_textobjects = false
 return {
 	"nvim-treesitter/nvim-treesitter",
-	version = false, -- last release is way too old and doesn't work on Windows
+  tag = 'v0.9.2',
 	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
