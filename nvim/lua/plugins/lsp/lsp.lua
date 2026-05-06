14b8ea264873c917e1b0dd95e6cd1085e8ac14af
Max Kasperowicz <yet3.dev@gmail.com>
2026-05-06 19:55:38 +0200

    feat: add nil check

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -31,7 +31,7 @@ return {
 
 				local client_id = args.data.client_id
 				local client = vim.lsp.get_client_by_id(client_id)
-				if client.name == "svelte-ls" then
+				if client and client.name == "svelte-ls" then
 					vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 						pattern = { "*.js", "*.ts" },
 						callback = function(ctx)
