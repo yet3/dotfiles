aa64530696257e84d30a890a2a8e6c6ebf2b61b9
Max Kasperowicz <yet3.dev@gmail.com>
2026-08-18 18:14:58 +0200

    feat: theme

--- nvim/lua/plugins/ui/theme.lua
+++ nvim/lua/plugins/ui/theme.lua
@@ -1,33 +1,44 @@
 return {
-	"craftzdog/solarized-osaka.nvim",
-	lazy = false,
-	priority = 99999,
-	config = function()
-		require("solarized-osaka").setup({
-			transparent = false,
-			terminal_colors = true,
-			styles = {
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = {},
-				variables = {},
-				sidebars = "dark",
-				floats = "dark",
-			},
-			sidebars = { "qf", "help" },
-			day_brightness = 0.3,
-			hide_inactive_statusline = false,
-			dim_inactive = false,
-			lualine_bold = false,
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		priority = 99999,
+		config = function()
+			require("solarized-osaka").setup({
+				transparent = false,
+				terminal_colors = true,
+				styles = {
+					comments = { italic = true },
+					keywords = { italic = true },
+					functions = {},
+					variables = {},
+					sidebars = "dark",
+					floats = "dark",
+				},
+				sidebars = { "qf", "help" },
+				day_brightness = 0.3,
+				hide_inactive_statusline = false,
+				dim_inactive = false,
+				lualine_bold = false,
 
-      ---@param colors ColorScheme
-			on_highlights = function(highlights, colors)
-				highlights.BlinkCmpMenu = { bg = colors.bg_float }
-				highlights.BlinkCmpMenuBorder = { bg = colors.bg_float }
-				highlights.BlinkCmpSource = { bg = colors.bg_float }
-			end,
-		})
+				---@param colors ColorScheme
+				on_highlights = function(highlights, colors)
+					highlights.BlinkCmpMenu = { bg = colors.bg_float }
+					highlights.BlinkCmpMenuBorder = { bg = colors.bg_float }
+					highlights.BlinkCmpSource = { bg = colors.bg_float }
+				end,
+			})
 
-		vim.cmd("colorscheme solarized-osaka")
-	end,
+			vim.cmd("colorscheme solarized-osaka")
+		end,
+	},
+	-- {
+	-- 	"lalitmee/cobalt2.nvim",
+	-- 	dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
+	-- 	lazy = false,
+	-- 	priority = 99999,
+	-- 	init = function()
+	-- 		require("colorbuddy").colorscheme("cobalt2")
+	-- 	end,
+	-- },
 }
