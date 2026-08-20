1dfac782e2fdbbb75958011c534f19bfe3f37d41
Max Kasperowicz <yet3.dev@gmail.com>
2026-08-20 23:54:17 +0200

    feat: disable tsserver logging

--- nvim/lsp/ts_ls.lua
+++ nvim/lsp/ts_ls.lua
@@ -1,9 +1,8 @@
 return {
 	init_options = {
 		tsserver = {
-			-- Tell the internal tsserver process where to spit out its raw log files
 			logDirectory = vim.fn.expand("~/.local/state/nvim/ts_ls_logs/"),
-			logVerbosity = "verbose", -- Options: "terse", "normal", "verbose"
+			logVerbosity = "off",
 		},
 	},
 }
